d3371a968abcab140aa5f4a8afae8f1566066d8a
GautamCyborg <9907230583gautam@gmail.com>
2023-12-15 12:53:52 +0530

    first commit

---
+++ frotend/src/Terms&Condtions/Aboutus.jsx
@@ -0,0 +1,176 @@
+import React from "react";
+import { Typography, Container, Grid, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import MetaData from "../component/layouts/MataData/MataData";
+import { Link } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  about_us: {
+    paddingTop: "8rem",
+    paddingBottom: "4rem",
+    backgroundColor: "white !important",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  container_12: {
+    padding: "2rem",
+    textAlign: "center",
+
+    backgroundColor: "white !important",
+    maxWidth: "100%",
+  },
+  image_about: {
+    width: "100%",
+    height: "auto",
+    marginTop: "3rem",
+    marginBottom: "2rem",
+  },
+  title_about: {
+    color: "#414141",
+    fontSize: "14px",
+    padding: "2rem 1rem 2rem",
+    fontFamily: "Roboto",
+    fontWeight: "500 !important",
+  },
+  heading12_about: {
+    fontSize: "1rem",
+    padding: "2rem 1rem 2rem",
+    fontWeight: "400 !important",
+    color: "#414141",
+    textAlign: "center",
+  },
+  introText_about: {
+    maxWidth: "800px",
+
+    lineHeight: "1.5",
+    margin: "1.5rem 0",
+    color: "#292929",
+    fontSize: "1.2rem",
+    fontWeight: "400 !important",
+    textAlign: "justify",
+    padding: "0.8rem 1rem",
+  },
+  infoText_about: {
+    lineHeight: "1.5",
+    margin: "2rem 0",
+    color: "#292929",
+    fontSize: "1rem",
+    fontWeight: "400 !important",
+    textAlign: "justify",
+    padding: "0.8rem 1rem",
+  },
+  buttonContainer_about: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "1rem 0",
+    width: "100%",
+    marginTop: "1rem",
+  },
+  button1_about: {
+    backgroundColor: "#000000 !important",
+    color: "white !important",
+    width: "fit-content !important",
+    padding: "0.8rem 2rem   !important",
+    marginLeft: "3.3rem !important",
+    borderRadius: "5px !important",
+    "&:hover": {
+      backgroundColor: "#ed1c24 !important",
+      color: "white !important",
+    },
+  },
+  button2_about: {
+    backgroundColor: "#292929 !important",
+    color: "white   !important",
+    width: "fit-content     !important",
+    padding: "0.8rem 2rem   !important",
+    marginLeft: "1.3rem !important",
+    borderRadius: "5px !important",
+    "&:hover": {
+      backgroundColor: "#ed1c24 !important",
+      color: "white !important",
+    },
+  },
+}));
+
+const About_UsPage = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.about_us}>
+        <MetaData title={"About Us"} />
+        <Container className={classes.container_12}>
+          <Grid container spacing={4} justifyContent="center">
+           
+            <Grid item xs={12} sm={6}>
+              <Typography
+                variant="h2"
+                component="h1"
+                className={classes.title_about}
+              >
+                About Us
+              </Typography>
+              <Typography variant="body1" className={classes.introText_about}>
+              Welcome to eShopp, your premier online shopping destination for an unparalleled retail experience. 
+              Whether you're a tech enthusiast, a fashion-forward trendsetter, a home decor aficionado, or just 
+              about anyone seeking quality products, we have you covered.
+              At eShopp, we've redefined the art of online shopping. We offer an extensive range of products, from 
+              the latest gadgets and designer apparel to stylish furniture and everyday essentials. Our user-friendly 
+              interface allows you to explore diverse categories with ease and find what you need quickly.
+              </Typography>
+              <Typography variant="body1" className={classes.introText_about}>
+              Why choose us? Here's what sets us apart:
+              <br></br>
+              1. Premium Selection: We curate a vast collection of products from trusted brands and vendors, ensuring you access the best quality and variety.
+              <br></br>
+              2. Competitive Prices: Enjoy competitive pricing and exclusive deals on a wide array of items, giving you the best value for your money.
+              <br></br>
+              3. Secure Shopping: Shop with confidence. We prioritize your security with robust encryption and secure payment options.
+              <br></br>
+              4. Fast and Reliable Delivery: We understand your need for quick deliveries. Rest assured, your orders will reach you in no time.
+              <br></br>
+              5. Dedicated Customer Support: Our customer support team is here to assist you with any inquiries, orders, or concerns.
+              <br></br>
+              <br></br>
+              eShopp is where your shopping journey begins. Explore the latest trends, upgrade your lifestyle, and enjoy the convenience of shopping from the comfort of your home. We're here to make every purchase a delightful experience.
+              Start shopping with us today and discover a world of possibilities at your fingertips. Your satisfaction is our commitment.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+        <Container className={classes.container_12}>
+          <Typography
+            variant="h3"
+            component="h1"
+            className={classes.heading12_about}
+          >
+          </Typography>
+          <div className={classes.buttonContainer_about}>
+            <Link
+              to="/products"
+              style={{ textDecoration: "none", color: "none" }}
+            >
+              <Button variant="contained" className={classes.button1_about}>
+                Our Products
+              </Button>
+            </Link>
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", color: "none" }}
+            >
+              <Button variant="contained" className={classes.button2_about}>
+                Contact Us
+              </Button>
+            </Link>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default About_UsPage;
